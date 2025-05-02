975884ebcdbdcd95b30df2118f5943a8c04162ac
jashanpratapsingh <jashanpratap123@gmail.com>
2025-05-01 23:40:04 -0400

    Updated the backend

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["http://localhost:5173"],  # React dev server
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Initialize the resume analyzer
